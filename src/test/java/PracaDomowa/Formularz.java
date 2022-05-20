b3157cd3c8df694805a7cbcef10744aa3e67d0c2
martynafoksinska <martyna.a.foksinska@gmail.com>
2022-05-20 20:33:47 +0200

    Exercise 3 Selenium done

--- src/test/java/PracaDomowa/Formularz.java
+++ src/test/java/PracaDomowa/Formularz.java
@@ -16,11 +16,15 @@ public class Formularz {
         driver.manage().window().maximize();
         driver.get("https://katalon-test.s3.amazonaws.com/demo-aut/dist/html/form.html");
 
-        WebElement signIn = driver.findElement(By.id("_desktop_user_info"));
-        signIn.click();
-        WebElement creatingAccount = driver.findElement(By.xpath("//*[@id='content']/div/a"));
-        creatingAccount.click();
-        driver.navigate().back();
-        driver.navigate().back();
+        driver.findElement(By.id("first-name")).sendKeys("Karol");
+        driver.findElement(By.id("last-name")).sendKeys("Kowalski");
+        driver.findElement(By.name("gender")).click();
+        driver.findElement(By.id("dob")).sendKeys("05/22/2010");
+        driver.findElement(By.id("address")).sendKeys("Prosta 51");
+        driver.findElement(By.id("email")).sendKeys("karol.kowalski@mailinator.com");
+        driver.findElement(By.id("password")).sendKeys("Pass123");
+        driver.findElement(By.id("company")).sendKeys("Coders Lab");
+        driver.findElement(By.id("comment")).sendKeys("To jest mój pierwszy automat testowy");
+        driver.findElement(By.id("submit")).click();
     }
 }
